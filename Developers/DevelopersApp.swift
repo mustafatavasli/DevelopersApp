2568654262fdb45827cbaed9024c52b585e2e01d
mustafatavasli <musstafatavasli@gmail.com>
2024-01-31 23:07:55 +0300

    Initial Commit

---
+++ Developers/DevelopersApp.swift
@@ -0,0 +1,17 @@
+//
+//  DevelopersApp.swift
+//  Developers
+//
+//  Created by Mustafa TAVASLI on 31.01.2024.
+//
+
+import SwiftUI
+
+@main
+struct DevelopersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
